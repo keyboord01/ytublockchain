35ea393a115fdb613f0c1866e87aff6d41b857f0
Ahmed Zeyad <keyboord01@gmail.com>
2025-01-20 17:54:58 +0300

    about section fix

--- components/About.tsx
+++ components/About.tsx
@@ -31,25 +31,6 @@ const About = () => {
         borderRadius: 0,
       });
     });
-
-    mm.add("(max-width: 767px)", () => {
-      const clipAnimation = gsap.timeline({
-        scrollTrigger: {
-          trigger: "#clip",
-          start: "center center",
-          end: "+=400 center",
-          scrub: 0.5,
-          pin: true,
-          pinSpacing: true,
-        },
-      });
-
-      clipAnimation.to(".sliding-image", {
-        width: "100vw",
-        height: "100vh",
-        borderRadius: 0,
-      });
-    });
   });
 
   return (
@@ -64,13 +45,12 @@ const About = () => {
             A thriving community of <br className="hidden md:block" />{" "}
             blockchain enthusiasts
           </h2>
-
           <p className="mx-auto mb-8 mt-4 max-w-2xl text-center text-lg text-gray-600">
             Join us in building the future of Web3 through innovation,
             collaboration, and continuous learning
           </p>
 
-          <div className="grid grid-cols-1 gap-14 p-6 md:grid-cols-2 lg:grid-cols-4 ">
+          <div className="grid grid-cols-1 gap-14 p-6 md:grid-cols-2 lg:grid-cols-4">
             <StatisticCard
               icon={Users}
               value={1400}
@@ -101,7 +81,7 @@ const About = () => {
             />
           </div>
 
-          <div className="subtext translate-y-12 sm:translate-y-0">
+          <div className="subtext translate-y-12 sm:translate-y-0 md:block hidden ">
             <p>
               Our blockchain club is more than just numbers. We&apos;re a
               community dedicated
@@ -114,10 +94,10 @@ const About = () => {
           </div>
         </div>
         <div
-          className="h-dvh w-screen pb-8 md:pb-0"
+          className="h-dvh w-screen pb-8 md:pb-0 md:block hidden "
           id="clip"
         >
-          <div className="sliding-image">
+          <div className="sliding-image left-0 translate-x-0 md:left-1/2 md:-translate-x-1/2 h-[60vh] w-96 origin-center overflow-hidden rounded-3xl md:w-[30vw]">
             <Image
               src="/img/about.png"
               alt="Background"
@@ -125,6 +105,32 @@ const About = () => {
               className="object-cover"
               priority
             />
+          </div>
+        </div>
+        <div className="relative h-[70vh] translate-y-1 w-screen md:hidden block">
+          <div className="absolute inset-0 rounded-t-lg overflow-hidden">
+            <Image
+              src="/img/about.png"
+              alt="Background"
+              fill
+              className="object-cover"
+              priority
+            />
+            <div className="absolute inset-0 bg-black/10"></div>
+          </div>
+
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="font-circular-web text-lg tracking-tighter text-center bg-black/60 backdrop-blur-[1px] shadow-xl rounded-sm py-2 px-2 mx-2">
+              <p className=" text-white">
+                Our blockchain club is more than just numbers. We&apos;re a
+                community dedicated
+              </p>
+              <p className="text-gray-400">
+                to learning, building, and innovating together. Whether
+                you&apos;re a beginner or an experienced developer, there&apos;s
+                a place for you here.
+              </p>
+            </div>
           </div>
         </div>
       </div>
