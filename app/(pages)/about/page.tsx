7fd8104ed8cf7eeef545687224d986cfee176c4d
Ahmed Zeyad <keyboord01@gmail.com>
2025-02-13 23:44:02 +0300

    update descriptions

---
+++ app/(pages)/about/page.tsx
@@ -0,0 +1,21 @@
+// import AboutUsSection from "@/components/about/AboutUsSection";
+// import Footer from "@/components/Footer";
+// import NavBar from "@/components/NavBar";
+
+// function App() {
+//   return (
+//     <main className="relative w-screen overflow-x-hidden">
+//       <NavBar />
+//       <AboutUsSection />
+//       <Footer />
+//     </main>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
+export const page = () => {
+  return <div>404 lol</div>;
+};
