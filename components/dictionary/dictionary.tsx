69c0d6481ca9208d149d5b0538817c6f20e93335
Ahmed Zeyad <keyboord01@gmail.com>
2025-03-27 13:47:10 +0300

    fix: remove key down bug

--- components/dictionary/dictionary.tsx
+++ components/dictionary/dictionary.tsx
@@ -89,20 +89,6 @@ const BlockchainDictionary = () => {
     Object.entries(groupedTerms).map(([letter, terms]) => [letter, terms])
   );
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      const searchTerm = searchQuery.trim().toLowerCase();
-      if (!searchTerm) return;
-
-      const match = dictionaryTerms.find((term) =>
-        term.term.toLowerCase().includes(searchTerm)
-      );
-
-      if (match) {
-        setSelectedTerm(match);
-      }
-    }
-  };
   return (
     <div className="min-h-screen text-white">
       <div className="relative h-screen flex flex-col items-center justify-center text-center p-4">
@@ -133,7 +119,6 @@ const BlockchainDictionary = () => {
             className="w-full p-2 px-4 bg-transparent rounded-full border border-gray-700 text-start outline-none"
             value={searchQuery}
             onChange={handleSearchChange}
-            onKeyDown={handleKeyPress}
           />
 
           {searchSuggestions.length > 0 && (
